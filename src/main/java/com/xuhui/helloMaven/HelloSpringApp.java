7778ffb59e63f6eefa5251605a6825321fadc531
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-04 21:15:17 -0800

    moved applicationContext.xml

--- src/main/java/com/xuhui/helloMaven/HelloSpringApp.java
+++ src/main/java/com/xuhui/helloMaven/HelloSpringApp.java
@@ -3,17 +3,18 @@ package com.xuhui.helloMaven;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
-		Path path = FileSystems.getDefault().getPath("src/applicationContext.xml").toAbsolutePath();
+		Path path = FileSystems.getDefault().getPath("src/main/java/applicationContext.xml").toAbsolutePath();
 		System.out.println("current path: " + path.toString());
-		
+//
 		// load the spring configuration file
-		ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("src/applicationContext.xml");
+		FileSystemXmlApplicationContext context =
+				new FileSystemXmlApplicationContext("src/main/java/applicationContext.xml");
 				
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
