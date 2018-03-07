80aa6981eb4d0a71f642247e5d0ad3be95bf1919
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-06 20:01:03 -0800

    added postconstruct

--- src/main/java/com/xuhui/demoAnnotations/AnnotationDemoApp.java
+++ src/main/java/com/xuhui/demoAnnotations/AnnotationDemoApp.java
@@ -17,6 +17,5 @@ public class AnnotationDemoApp {
 
         // close the context
         context.close();
-
     }
 }
--- src/main/java/com/xuhui/demoAnnotations/TennisCoach.java
+++ src/main/java/com/xuhui/demoAnnotations/TennisCoach.java
@@ -1,9 +1,12 @@
 package com.xuhui.demoAnnotations;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class TennisCoach implements Coach{
@@ -15,6 +18,18 @@ public class TennisCoach implements Coach{
     /* default constructor */
     public TennisCoach() {
         System.out.println(">> TennisCoach: Inside the default constructor");
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside doMyStartupStuff");
+    }
+
+    // define my destroy method - JDK 9.0 problems waiting to be resolved
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside doMyStartupStuff");
     }
 
     @Override
