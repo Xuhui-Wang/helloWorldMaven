639fac8cb9ac703243650926a93617e2ab606302
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-07 08:33:43 -0800

    literal injection from property files

--- src/main/java/com/xuhui/demoAnnotations/SportConfig.java
+++ src/main/java/com/xuhui/demoAnnotations/SportConfig.java
@@ -3,9 +3,10 @@ package com.xuhui.demoAnnotations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-//@ComponentScan("com.xuhui.demoAnnotations")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
     // define bean for SadFortuneService
     @Bean
--- src/main/java/com/xuhui/demoAnnotations/SwimJavaConfigDemoApp.java
+++ src/main/java/com/xuhui/demoAnnotations/SwimJavaConfigDemoApp.java
@@ -8,11 +8,15 @@ public class SwimJavaConfigDemoApp {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
         // retrieve bean from spring container
-        Coach theCoach = context.getBean("swimCoach", Coach.class);
+        SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 
         // call methods on the bean
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
+
+        // call our new swim methods ... how the prop values injected
+        System.out.println(theCoach.getEmail());
+        System.out.println(theCoach.getTeam());
 
         // close the context
         context.close();
