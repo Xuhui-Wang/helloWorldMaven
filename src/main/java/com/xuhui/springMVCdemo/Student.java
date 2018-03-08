dddb44b8fa1895b5c4350dcf78b8c58f59492fa4
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-08 12:03:21 -0800

    refactor - read Map from properties file

--- src/main/java/com/xuhui/springMVCdemo/Student.java
+++ src/main/java/com/xuhui/springMVCdemo/Student.java
@@ -1,20 +1,12 @@
 package com.xuhui.springMVCdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Student {
     private String firstName, lastName, country;
-    private LinkedHashMap<String, String> countryOptions;
-
-    public Student() {
-        // populate country options
-        countryOptions = new LinkedHashMap<>();
-        countryOptions.put("BR", "Brazil");
-        countryOptions.put("FR", "France");
-        countryOptions.put("DE", "Germany");
-        countryOptions.put("IN", "India");
-        countryOptions.put("US", "United States");
-    }
 
     public String getFirstName() {
         return firstName;
@@ -38,9 +30,5 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
-    }
-
-    public LinkedHashMap<String, String> getCountryOptions() {
-        return countryOptions;
     }
 }
--- src/main/java/com/xuhui/springMVCdemo/StudentController.java
+++ src/main/java/com/xuhui/springMVCdemo/StudentController.java
@@ -1,13 +1,20 @@
 package com.xuhui.springMVCdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+
+    @Value("#{countryOptions}")
+    private Map<String, String> countryOptions;
+
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
         // create a new Student object
@@ -15,6 +22,9 @@ public class StudentController {
 
         // add the object to the model
         theModel.addAttribute("student", student);
+
+    // add the country options to the model
+        theModel.addAttribute("theCountryOptions", countryOptions);
 
         return "student-form";
     }
