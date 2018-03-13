012aaf180ca035a41defa4f9c9c7a571bb49d207
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-13 16:59:08 -0700

    created - CreateCourseDemo

--- src/main/java/com/xuhui/hibernate/demo/hb_03_one_to_many/CreateCoursesDemo.java
+++ src/main/java/com/xuhui/hibernate/demo/hb_03_one_to_many/CreateCoursesDemo.java
@@ -21,30 +21,6 @@ public class CreateCoursesDemo {
             // start transaction
             session.beginTransaction();
 
-            // create an object
-            Instructor instructor = new Instructor("Mike", "Donald", "donald@whitehouse.gov");
-
-            InstructorDetail instructorDetail = new InstructorDetail("https://youtu.be/aaOB-ErYq6Y",
-                    "code in PHP");
-
-            // associate the objects
-            instructor.setInstructorDetail(instructorDetail);
-            session.save(instructor);
-/*
-            // create an object
-            instructor = new Instructor("Bill", "Clinton", "clinton@whitehouse.gov");
-
-            instructorDetail = new InstructorDetail("https://youtu.be/aaOB-ErYq6Y",
-                    "code in Python");
-
-            // associate the objects
-            instructor.setInstructorDetail(instructorDetail);
-
-            // save the instructor (also save detail object)
-            session.save(instructor);
-            */
-            System.out.println("Saved the instructor: " + instructor);
-
             // commit the transaction
             session.getTransaction().commit();
 
