964ce42e517b8e21e972c32b7a1aa1457738c789
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-13 13:19:10 -0700

    create instructor

---
+++ src/main/java/com/xuhui/hibernate/demo/hb_02_bi/CreateDemo.java
@@ -0,0 +1,57 @@
+package com.xuhui.hibernate.demo.hb_02_bi;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start transaction
+            session.beginTransaction();
+
+            // create an object
+            Instructor instructor = new Instructor("Donald", "Trump", "trump@whitehouse.gov");
+
+            InstructorDetail instructorDetail = new InstructorDetail("https://youtu.be/aaOB-ErYq6Y",
+                    "code in Java");
+
+            // associate the objects
+            instructor.setInstructorDetail(instructorDetail);
+            session.save(instructor);
+
+            // create an object
+            instructor = new Instructor("Bill", "Clinton", "clinton@whitehouse.gov");
+
+            instructorDetail = new InstructorDetail("https://youtu.be/aaOB-ErYq6Y",
+                    "code in Python");
+
+            // associate the objects
+            instructor.setInstructorDetail(instructorDetail);
+
+            // save the instructor (also save detail object)
+            session.save(instructor);
+            System.out.println("Saved the instructor: " + instructor);
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
---
+++ src/main/java/com/xuhui/hibernate/demo/hb_02_bi/DeleteInstructorDetailDemo.java
@@ -0,0 +1,51 @@
+package com.xuhui.hibernate.demo.hb_02_bi;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructorDetailDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start transaction
+            session.beginTransaction();
+
+            // get the instructor detail object
+            int id = 2;
+            InstructorDetail instructorDetail =
+                    session.get(InstructorDetail.class, id);
+
+            // print the instructor detail
+            System.out.println("Instructor detail: " + instructorDetail);
+
+            // print the associated instructor
+            System.out.println("Associated instructor: " + instructorDetail.getInstructor());
+
+            // delete instructor detail
+            System.out.println("Deleting instructor detail: " + instructorDetail);
+            session.delete(instructorDetail);
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            // handle connection leak issue
+            session.close();
+            factory.close();
+        }
+    }
+}
