ff97eea2c9b409180fdee2d5cbc5d615e452bbd9
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-13 18:19:05 -0700

    get Instructor courses demo

--- src/main/java/com/xuhui/hibernate/demo/hb_03_one_to_many/GetInstructorCoursesDemo.java
+++ src/main/java/com/xuhui/hibernate/demo/hb_03_one_to_many/GetInstructorCoursesDemo.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateCoursesDemo {
+public class GetInstructorCoursesDemo {
     public static void main(String[] args) {
         // create session factory
         SessionFactory factory = new Configuration()
@@ -26,17 +26,7 @@ public class CreateCoursesDemo {
             Instructor instructor = session.get(Instructor.class, id);
             System.out.println("Get the instructor: " + instructor);
 
-            // create some courses
-            Course course1 = new Course("Football");
-            Course course2 = new Course("Ping-pong master class");
-            instructor.add(course1);
-            instructor.add(course2);
-
-            // save the courses
-            session.save(course1);
-            session.save(course2);
-//            session.save(instructor);
-
+            //
             // commit the transaction
             session.getTransaction().commit();
 
