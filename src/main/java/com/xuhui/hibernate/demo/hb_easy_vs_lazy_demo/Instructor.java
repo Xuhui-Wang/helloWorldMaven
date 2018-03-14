39e096f7ac965903163732c9a14290efb688aa3f
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-14 16:08:29 -0700

    don't print out courses

--- src/main/java/com/xuhui/hibernate/demo/hb_easy_vs_lazy_demo/Instructor.java
+++ src/main/java/com/xuhui/hibernate/demo/hb_easy_vs_lazy_demo/Instructor.java
@@ -106,7 +106,7 @@ public class Instructor {
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", instructorDetail=" + instructorDetail +
-                ", courses=" + courses +
+//                ", courses=" + courses +
                 '}';
     }
 }
