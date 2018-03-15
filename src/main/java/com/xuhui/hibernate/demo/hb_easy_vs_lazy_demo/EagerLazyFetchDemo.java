a29eb7beeb74a372a08c0e0dac88f39918ff4ce8
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-03-14 17:59:06 -0700

    Join fetch in HQL

---
+++ src/main/java/com/xuhui/hibernate/demo/hb_easy_vs_lazy_demo/EagerLazyFetchDemo.java
@@ -0,0 +1,44 @@
+package com.xuhui.hibernate.demo.hb_easy_vs_lazy_demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EagerLazyFetchDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start transaction
+            session.beginTransaction();
+
+            // get the instructor
+            int id = 1;
+            Instructor instructor = session.get(Instructor.class, id);
+            System.out.println("Get the instructor: " + instructor);
+            System.out.println("Courses: " + instructor.getCourses());
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+//            System.out.println("Courses: " + instructor.getCourses());
+
+            System.out.println("Done!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
--- src/main/java/com/xuhui/hibernate/demo/hb_easy_vs_lazy_demo/FetchJoinDemo.java
+++ src/main/java/com/xuhui/hibernate/demo/hb_easy_vs_lazy_demo/FetchJoinDemo.java
@@ -3,8 +3,9 @@ package com.xuhui.hibernate.demo.hb_easy_vs_lazy_demo;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
-public class GetInstructorCoursesDemo {
+public class FetchJoinDemo {
     public static void main(String[] args) {
         // create session factory
         SessionFactory factory = new Configuration()
@@ -21,16 +22,28 @@ public class GetInstructorCoursesDemo {
             // start transaction
             session.beginTransaction();
 
-            // get the instructor
+            // option 2: hibernate query with HQL
+            // get the instructor from db
             int id = 1;
-            Instructor instructor = session.get(Instructor.class, id);
+            Query<Instructor> query =
+                    session.createQuery("select i from Instructor i "
+                            + "JOIN FETCH i.courses "
+                            + "WHERE i.id =:instructorId",
+                            Instructor.class);
+
+            // set query parameter
+            query.setParameter("instructorId", id);
+
+            // execute query and get parameter
+            Instructor instructor = query.getSingleResult();
+
             System.out.println("Get the instructor: " + instructor);
-            System.out.println("Courses: " + instructor.getCourses());
 
             // commit the transaction
             session.getTransaction().commit();
 
-//            System.out.println("Courses: " + instructor.getCourses());
+            System.out.println("Session committed");
+            System.out.println("Courses: " + instructor.getCourses());
 
             System.out.println("Done!");
         } catch (Exception e) {
